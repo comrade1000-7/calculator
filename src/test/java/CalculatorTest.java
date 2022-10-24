4d06f7a14d7c6c76bf766e7174784daf2225d984
Vasily Parshentsev <vparshentsev@gmail.com>
2022-10-25 02:31:14 +0300

    Upgrade test methods

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,17 +1,26 @@
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 
-public class TestCalculator {
-        Calculator calculator = Calculator.instance.get();
+public class CalculatorTest {
+        Calculator calculator;
+
+        @BeforeEach
+        public void setup() {
+                calculator = Calculator.instance.get();
+        }
 
         @Test
-        public void testPlus() {
+        public void shouldSumNumbers() {
                 int expected = 5;
                 int actual = calculator.plus.apply(2, 3);
 
@@ -19,16 +28,23 @@ public class TestCalculator {
         }
 
         @ParameterizedTest
-        @CsvSource({"10, 7, 3","20, 15, 5"})
-        public void testMinus(int a, int b, int expected) {
+        @MethodSource("providesValuesForSubtract")
+        public void shouldSubtractNumbers(int a, int b, int expected) {
                 int actual = calculator.minus.apply(a, b);
 
                 assertThat(expected, equalTo(actual));
         }
 
+        public static Stream<Arguments> providesValuesForSubtract () {
+                return Stream.of(
+                        Arguments.of(10, 7, 3),
+                        Arguments.of(20, 15, 5)
+                );
+        }
+
         @ParameterizedTest
         @CsvFileSource(resources = "data.csv")
-        public void testMultiply(int a, int b, int expected) {
+        public void shouldMultiplyNumbers(int a, int b, int expected) {
 
                 int actual = calculator.multiply.apply(a, b);
 
@@ -37,14 +53,14 @@ public class TestCalculator {
 
         @ParameterizedTest
         @CsvFileSource(resources = "data2.csv")
-        public void testIsPositive(int a, boolean expected) {
+        public void shouldAnswerIsPositive(int a, boolean expected) {
                 boolean actual = calculator.isPositive.test(a);
 
                 assertThat(expected, equalTo(actual));
         }
 
         @Test
-        public void testAbs() {
+        public void shouldReturnPositiveNumber() {
                 int expected = 5;
                 int actual = calculator.abs.apply(-5);
 
